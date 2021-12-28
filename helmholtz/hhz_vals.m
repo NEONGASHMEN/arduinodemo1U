90f08354edc9a3afc86991e3d3d50ca5051fe5a4
NEONGASHMEN <83530865+NEONGASHMEN@users.noreply.github.com>
2021-12-28 16:15:42 +0530

    Migrate to SWG

--- helmholtz/hhz_vals.m
+++ helmholtz/hhz_vals.m
@@ -1,27 +1,26 @@
-
 clear
 clc
 close all
  
 %%To calculate the magnetic field at the centre of a helmholtz coil
  
-%fid = fopen('data.txt', 'w');
-%fprintf(fid,'%4s\t\t %4s\t\t %4s\t\t %4s\t\t  %4s\t\t', 'Current', 'No of turns', 'Length of wire', 'Resistance', 'Power' );                %  X-axis common time
+% fid = fopen('data.txt', 'w');
+% fprintf(fid,'%4s\t\t %4s\t\t %4s\t\t %4s\t\t  %4s\t\t', 'Current', 'No of turns', 'Length of wire', 'Resistance', 'Power' );                %  X-axis common time
  
 %Formula B = [0.8991*(10)^(-6)*(n*I)]/R
  
 %mu = 12.56e-7;
 k = 0.8991e-6;      
-r = 0.25;           %radius of coil in metre
-B = 0.008;          %magnetic field in gauss
+r = 0.265;           %radius of coil in metre
+B = 0.003;          %magnetic field in Tesla
  
 rho = 1.68e-8;      %resistivity of copper
 A_wire = 2.62e-6;   %area of 13 gauge wire
 n = 0;
 j=1;
 
-areaArray = [10.6 8.36 6.63 5.26 4.17 3.31 2.63 2.08 1.65 1.31 1.04 0.82 0.65 0.52];
-guageArray = 7:20;
+guage = [7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32];
+areaArray = [15.6958 12.9717 10.5071 8.3019 6.8183 5.4805 4.2888 3.2429 2.6268 2.0755 1.5890 1.1675 0.8107 0.6567 0.5189 0.3973 0.2919 0.2452 0.2027 0.1642 0.1363 0.1110 0.0937 0.0779 0.0682 0.0591];
 wires = cell(5,length(areaArray));
 
 
@@ -38,7 +37,7 @@ for z = 1:length(areaArray)
 
       P(j) = I(j)*I(j)*R(j);                 %Power required
 
-      %fprintf(fid,'\n %4.4f \t\t %4.4f \t\t %4.4f \t\t %4.4f \t\t %4.4f\t\t', I(j), n(j), L_wire(j), R(j), P(j));      
+%       fprintf(fid,'\n %4.4f \t\t %4.4f \t\t %4.4f \t\t %4.4f \t\t %4.4f\t\t', I(j), n(j), L_wire(j), R(j), P(j));      
       
       
       j=j+1;
@@ -55,13 +54,45 @@ for z = 1:length(areaArray)
 end
  
 for i = 1:length(areaArray)
-    srtr = strcat(num2str(guageArray(i)),'.txt');
+    srtr = strcat(num2str(guage(i)),'.txt');
     fid = fopen(srtr,'w');
     fprintf(fid,'%4s\t\t %4s\t\t %4s\t\t %4s\t\t  %4s\t\t\r\r', 'Current', 'No of turns', 'Length of wire', 'Resistance', 'Power' );
     for j = 1:length(I)
         fprintf(fid,'%4.1f \t\t %4.4f \t\t %4.4f \t\t %4.4f \t\t %4.4f\t\t\r',wires{1,i}(j),wires{2,i}(j),wires{3,i}(j),wires{4,i}(j),wires{5,i}(j));
     end
 end
+
+wht2plot = [10,11,12,13,14,15,16];      %%put the guages that are to be plotted
+for i = wht2plot
+    
+    srtr = strcat(num2str(guage(i-6)),' guage wire');
+    sid = figure('Name',srtr,'NumberTitle','off');
+    
+    subplot(3,1,1);
+    title('Current V No of turns');
+    plot(wires{2,i-6},wires{1,i-6},'r','LineWidth',2);
+    xlabel('No of turns (nos)');
+    ylabel('Current (A)');
+    
+    subplot(3,1,2);
+    title('Power V No of turns');
+    plot(wires{2,i-6},wires{5,i-6},'b','LineWidth',2);
+    xlabel('No of turns (nos)');
+    ylabel('Power (watts)');
+    
+    subplot(3,1,3);
+    title('Length V No of turns');
+    plot(wires{2,i-6},wires{3,i-6},'Color','#1d7526','LineWidth',2);
+    xlabel('No of turns (nos)');
+    ylabel('Length (m)');
+    
+    hold on
+    
+end
+ 
+disp('simulation complete')
+
+
 
 % figure(1)
 % subplot(3,1,1)
@@ -93,37 +124,9 @@ end
 % %xlim([0 2]);
 % %set(gca,'YTick',480:50:650);
 % grid;
-%  
-%  
-% %figure(1);
-% %plot(I,n);
+ 
+ 
+%figure(1);
+%plot(I,n);
 
-wht2plot = 1:4:length(guageArray);
-for i = wht2plot
-    
-    srtr = strcat(num2str(guageArray(i)),' guage wire');
-    sid = figure('Name',srtr,'NumberTitle','off');
-    
-    subplot(3,1,1);
-    title('Current V No of turns');
-    plot(wires{2,i},wires{1,i},'r','LineWidth',2);
-    xlabel('No of turns (nos)');
-    ylabel('Current (A)');
-    
-    subplot(3,1,2);
-    title('Power V No of turns');
-    plot(wires{2,i},wires{5,i},'b','LineWidth',2);
-    xlabel('No of turns (nos)');
-    ylabel('Power (watts)');
-    
-    subplot(3,1,3);
-    title('Length V No of turns');
-    plot(wires{2,i},wires{3,i},'Color','#1d7526','LineWidth',2);
-    xlabel('No of turns (nos)');
-    ylabel('Length (m)');
-    
-    hold on
-    
-end
 
-disp('simulation complete')
