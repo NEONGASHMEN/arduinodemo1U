01d2ac59c1d1391918ca84d8f567598c040b6a02
NEONGASHMEN <83530865+NEONGASHMEN@users.noreply.github.com>
2022-01-17 00:13:53 +0530

    Update Control.m

--- detumblingSim-MatLab/Control.m
+++ detumblingSim-MatLab/Control.m
@@ -1,13 +1,12 @@
 function muB = Control(Bfieldnav,pqrdotnav)
     Magtorquer_params
     
-    k = 650;
+    k = 650;        %650
 
     WxB = cross(pqrdotnav,Bfieldnav);
-%     muBhat = WxB/norm(WxB);
-%     muB = muBnorm*muBhat;
 
     muB = k*WxB;
+%     muB = 0.2*(WxB/norm(WxB));
     
     if sum(abs(muB)) > 0.2
         muB = (muB/norm(muB))*0.2;
